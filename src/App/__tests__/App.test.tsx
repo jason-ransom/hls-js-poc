2b30d0201bab5cd89167af160dd1e6bb967b2ebb
Jason Ransom <jasonlawrenceransom@gmail.com>
2024-04-15 11:18:29 -0400

    added vitest

---
+++ src/App/__tests__/App.test.tsx
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import App from "../App.tsx";
+
+describe('App', () => {
+    it('renders as expected', () => {
+        const { getByText } = render(<App />);
+
+        getByText('HLS.JS POC');
+    });
+});
