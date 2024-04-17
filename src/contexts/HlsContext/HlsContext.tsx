693e127442b39b2a375b5f384e71634b70f5fb33
Jason Ransom <jasonlawrenceransom@gmail.com>
2024-04-16 23:02:56 -0400

    added HlsContext values.

--- src/contexts/HlsContext/HlsContext.tsx
+++ src/contexts/HlsContext/HlsContext.tsx
@@ -1,18 +1,44 @@
-import { FC, ReactNode, createContext } from 'react';
+import { FC, ReactNode, createContext, useState } from 'react';
 import Hls from 'hls.js';
 
 type HlsContextContainerProps = {
   children: ReactNode;
 };
 
-const HlsContext = createContext({});
+type HlsContextProps = {
+  isSupported: boolean;
+  attachedVideoElement: HTMLVideoElement | null;
+  updateAttachedVideoElement: (attachedVideoElement?: HTMLVideoElement) => void;
+};
+
+const HlsContext = createContext<HlsContextProps>({
+  isSupported: false,
+  attachedVideoElement: null,
+  updateAttachedVideoElement: () => null,
+});
 
 const HlsContextContainer: FC<HlsContextContainerProps> = ({ children }) => {
+  const [attachedVideoElement, setAttachedVideoElement] = useState<HTMLVideoElement | null>(null);
+
+  const handleUpdateAttachedVideoElement = (videoElement: HTMLVideoElement | null = null) => {
+    setAttachedVideoElement(videoElement);
+  };
+
   if (!Hls.isSupported()) {
     return <div>Error! Media Source Extensions is not available.</div>;
   }
 
-  return <HlsContext.Provider value={{}}>{children}</HlsContext.Provider>;
+  return (
+    <HlsContext.Provider
+      value={{
+        isSupported: Hls.isSupported(),
+        attachedVideoElement,
+        updateAttachedVideoElement: handleUpdateAttachedVideoElement,
+      }}
+    >
+      {children}
+    </HlsContext.Provider>
+  );
 };
 
 export default HlsContextContainer;
