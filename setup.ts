e883e64e5830a352f0d3d546cad90801c2ae0143
Jason Ransom <jasonlawrenceransom@gmail.com>
2024-04-18 20:33:49 -0400

    added light testing around hls instance setup.

--- setup.ts
+++ setup.ts
@@ -3,16 +3,21 @@ import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
 
 // mock hls.js library globally
-class MockHljs {
-  constructor() {}
+export const mockHljsConstructor = vi.fn();
+export const mockAttachMedia = vi.fn();
+export const mockOn = vi.fn();
+export class MockHljs {
+  constructor() {
+    mockHljsConstructor();
+  }
 
   static Events = {
     MEDIA_ATTACHED: 'MEDIA_ATTACHED',
   };
   static isSupported = vi.fn(() => true);
 
-  attachMedia = vi.fn();
-  on = vi.fn();
+  attachMedia = mockAttachMedia;
+  on = mockOn;
 }
 vi.mock('hls.js', () => ({
   default: MockHljs,
--- src/contexts/HlsContext/__tests__/hlsContext.test.tsx
+++ src/contexts/HlsContext/__tests__/hlsContext.test.tsx
@@ -1,16 +1,29 @@
 import { render } from '@testing-library/react';
 import { describe, expect, it, Mock } from 'vitest';
 import Hls from 'hls.js';
-import HlsContext from '../HlsContext.tsx';
+import HlsContextContainer, { HlsContext } from '../HlsContext.tsx';
+import { useContext, useEffect } from 'react';
+import { mockAttachMedia, mockHljsConstructor } from '../../../../setup.ts';
 
 const EXPECTED_ERROR_MESSAGE = 'Error! Media Source Extensions is not available.';
 const CONTENT = 'content';
+
+const videoElement = document.createElement('video');
+const TestComponent = () => {
+  const { updateAttachedVideoElement } = useContext(HlsContext);
+
+  useEffect(() => {
+    updateAttachedVideoElement(videoElement);
+  }, [updateAttachedVideoElement]);
+
+  return null;
+};
 
 describe('HlsContext', () => {
   it('renders error message when MediaSource Extensions are not available', () => {
     (Hls.isSupported as Mock).mockReturnValue(false);
 
-    const { queryByText } = render(<HlsContext>{CONTENT}</HlsContext>);
+    const { queryByText } = render(<HlsContextContainer>{CONTENT}</HlsContextContainer>);
 
     expect(queryByText(EXPECTED_ERROR_MESSAGE)).toBeInTheDocument();
     expect(queryByText(CONTENT)).not.toBeInTheDocument();
@@ -19,11 +32,23 @@ describe('HlsContext', () => {
   it('renders expected content MediaSource Extensions are available', () => {
     (Hls.isSupported as Mock).mockReturnValue(true);
 
-    const { queryByText } = render(<HlsContext>{CONTENT}</HlsContext>);
+    const { queryByText } = render(<HlsContextContainer>{CONTENT}</HlsContextContainer>);
 
     expect(queryByText(EXPECTED_ERROR_MESSAGE)).not.toBeInTheDocument();
     expect(queryByText(CONTENT)).toBeInTheDocument();
   });
 
-  it.todo('instantiates HLS and binds it to video when available');
+  it('instantiates HLS and binds it to video when available', () => {
+    render(
+      <HlsContextContainer>
+        <TestComponent />
+      </HlsContextContainer>
+    );
+
+    // confirm mocked Hljs instance was created
+    expect(mockHljsConstructor).toHaveBeenCalledTimes(1);
+
+    // confirm video element attached
+    expect(mockAttachMedia).toHaveBeenCalledWith(videoElement);
+  });
 });
